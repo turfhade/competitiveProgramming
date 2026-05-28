1154c173e42cdb6698824daf66509efe2b9ba287
hadexyz <142442544+hadexyz@users.noreply.github.com>
2026-05-28 18:56:50 -0300

    add ICPC

---
+++ ICPC/SA/2025-1Phase-C.cpp
@@ -0,0 +1,24 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+void solve()
+{
+    
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int n = 1;
+    //cin >> n;
+    while(n--)
+    {
+        solve();
+    }
+}
