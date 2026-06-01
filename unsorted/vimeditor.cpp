f00bdb4c2aef0595818c61704599868f8d9601e6
hadexyz <142442544+hadexyz@users.noreply.github.com>
2026-06-01 14:45:06 -0300

    add

---
+++ unsorted/vimeditor.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+
+template <typename T>
+void read(vector<T> &a)
+{
+    for (auto &x : a)
+        cin >> x;
+}
+void solve()
+{
+    /*
+    ll n;
+    cin >> n;
+    vll arr(n);
+    read(arr);
+   */
+	for(ll i = 1; i < 5; i++)
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
