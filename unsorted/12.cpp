622695185f90b3072c751bab2b159e80b3ce8f2f
hadexyz <142442544+hadexyz@users.noreply.github.com>
2026-06-30 13:24:33 -0300

    add

---
+++ unsorted/12.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+
+int main()
+{
+    ll n;
+    vll in(n+1);
+    for(ll i = n; i >= 0; i--)
+    {
+        cin >> n[i];
+    }
+    while(in.size() != 1 && in[0] != 1)
+    {
+      
+    }
+    return 0;
+
+}
